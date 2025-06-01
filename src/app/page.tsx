1c3bb0ebc2a51369a12c3d3b23a7e97133d959cf
Isuru-Kottahachchi <isurukottasl@gmail.com>
2025-06-01 14:39:31 +0530

    Initial project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/componenets/hero/Hero";
+import NavBar from "@/componenets/navbar/NavBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <NavBar/>
+    <Hero/>
+    </>
+  );
+}
